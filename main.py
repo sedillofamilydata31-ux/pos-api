a42fe298963f4b2f36ca85232e50ffb9f1cecaec
sedillofamilydata31-ux <sedillofamilydata31@gmail.com>
2026-03-25 15:24:45 +0800

    main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS   # ADD THIS
 import os
 import json
 
 app = Flask(__name__)
+CORS(app)  # ADD THIS
 
 # ==============================
 # LOAD EXISTING DATA (PERSIST)
